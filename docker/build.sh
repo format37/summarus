c21060ca104f0eb24ccb33dbdbdf17ebcecfaa95
format37 <format37@gmail.com>
2021-05-28 16:56:15 +0300

    new file:   Dockerfile 	new file:   build.sh 	new file:   requirements.txt 	new file:   run.sh 	new file:   src/docker_init.py 	new file:   src/test_data.txt

---
+++ docker/build.sh
@@ -0,0 +1,4 @@
+sudo docker build . --tag format37/summarus
+#sudo docker-compose build
+spd-say "container build complete"
+#echo sudo docker-compose up
---
+++ docker/run.sh
@@ -0,0 +1,1 @@
+sudo docker run --name summarus --gpus all --net=host -e "port=6379" -d format37/summarus:latest
